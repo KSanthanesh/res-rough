bdbe0a26f977fa8ced75033ffaf58a1239b89691
KSanthanesh <santhanesh_2004@yahoo.com>
2021-09-27 20:56:35 +0000

    Add initial commit

---
+++ meals/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+# from cloudinary.models import CloudinaryField
+
+
+class Meals(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    preparation_time = models.IntegerField()
+    image = models.ImageField(upload_to='meals/', default=True)
+
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
+
+    def __str__(self):
+        return self.name
+
---
+++ meals/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Meals
+
+
+def get_list(request):
+    return render(request, 'list.html')
+
+
+# def get_meal_list(request):
+#     meal_list = Meals.objects.all()
+
+#     context = {
+#         'meal_list': meal_list,
+#     }
+
+#     return render(request, 'list.html', context)
+
+
+# def get_meal_detail(request,slug):
+#     meal_detail = Meals.objects.all()
+
+#     context = {
+#         'meal_detail': meal_detail,
+#     }
+#     return render(request, 'meals/detail.html', context)
